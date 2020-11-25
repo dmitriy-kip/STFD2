ece3628744ffd42422c78b928064b8856b561448
fed8055 <fed8055@yandex.ru>
2020-11-25 17:36:42 +0700

    добавил возможность открывать камеру

---
+++ app/src/main/java/com/example/stfd/PhotoSenderActivity.java
@@ -0,0 +1,30 @@
+package com.example.stfd;
+
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class PhotoSenderActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_sender);
+
+        Button cameraGo = findViewById(R.id.make_photo);
+        cameraGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+}
