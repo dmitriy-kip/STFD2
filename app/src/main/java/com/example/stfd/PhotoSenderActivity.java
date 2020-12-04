2bb6a4cc0f47dca539f0366deff1864c9cd49bbd
fed8055 <fed8055@yandex.ru>
2020-12-04 17:13:37 +0700

    добавил библиотеку для камеры, начал разбиратся с ресайкл вью

--- app/src/main/java/com/example/stfd/PhotoSenderActivity.java
+++ app/src/main/java/com/example/stfd/PhotoSenderActivity.java
@@ -1,16 +1,30 @@
 package com.example.stfd;
 
-import android.app.AppComponentFactory;
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.thorny.photoeasy.OnPictureReady;
+import com.thorny.photoeasy.PhotoEasy;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,60 +32,43 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PhotoSenderActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    String mCurrentPhotoPath;
-    Intent takePictureIntent;
     ImageView imageView;
+    PhotoEasy photoEasy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photo_sender);
 
+        photoEasy = PhotoEasy.builder()
+                .setActivity(this)
+                .setStorageType(PhotoEasy.StorageType.media)
+                .build();
+
+        imageView = findViewById(R.id.image);
+
         Button cameraGo = findViewById(R.id.make_photo);
         cameraGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        // ошибка, возникшая в процессе создания файла
-                    }
-                    if (photoFile != null) {
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                    }
-                }
+                photoEasy.startActivityForResult(PhotoSenderActivity.this);
             }
         });
 
-        imageView = findViewById(R.id.image);
+
     }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp;
-        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        //File image = File.createTempFile(imageFileName,  /* prefix */".jpg",/* suffix */storageDir/* directory */);
-        File image = new File(storageDir, imageFileName + ".jpg");
-        // Save a file: path for use with ACTION_VIEW intents
-        //mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-
-
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        photoEasy.onActivityResult(requestCode, resultCode, new OnPictureReady() {
+            @Override
+            public void onFinish(Bitmap thumbnail) {
+                imageView.setImageBitmap(thumbnail);
+            }
+        });
+
     }
+
+
 }
