ab2f82cb3cad5db2cbfe3322f7d22886a59e50e7
fed8055 <fed8055@yandex.ru>
2020-12-17 14:19:11 +0700

    add Volley and request metod

--- app/src/main/java/com/example/stfd/PhotoSenderActivity.java
+++ app/src/main/java/com/example/stfd/PhotoSenderActivity.java
@@ -1,60 +1,38 @@
 package com.example.stfd;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.loopj.android.http.TextHttpResponseHandler;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.thorny.photoeasy.OnPictureReady;
 import com.thorny.photoeasy.PhotoEasy;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import cz.msebera.android.httpclient.Header;
+import java.util.Map;
 
 public class PhotoSenderActivity extends AppCompatActivity {
     ImageView imageView;
@@ -62,8 +40,9 @@ public class PhotoSenderActivity extends AppCompatActivity {
     List<Bitmap> smallImages = new ArrayList<>();
     MyAdapter myAdapter;
     Context context = this;
-    ArrayList<File> listImages = new ArrayList<>();
+    ArrayList<String> listImages = new ArrayList<>();
     Button sendPhoto;
+    String encodeImageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,37 +65,28 @@ public class PhotoSenderActivity extends AppCompatActivity {
         sendPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AsyncHttpClient client = new AsyncHttpClient(true, 80, 443);
-                RequestParams params = new RequestParams();
-                try {
-                    params.put("file_upload", listImages.get(0), "application/octet-stream");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-                client.post("https://172.16.0.227:600/api/upload_file",params,new TextHttpResponseHandler(){
+                StringRequest request = new StringRequest(Request.Method.POST, "https://172.16.0.227:600/api/upload_file", new Response.Listener<String>() {
                     @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                        Log.e("ответ", "не ок " + responseString);
-                    }
+                    public void onResponse(String response) {
+                        Log.e("otvet", "ok" + response);
 
+                    }
+                }, new Response.ErrorListener() {
                     @Override
-                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                        Log.e("ответ","все ок " + responseString);
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("otvet", "ok" + error);
                     }
-
-                   /* @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                        // Root JSON in response is an dictionary i.e { "data : [ ... ] }
-                        // Handle resulting parsed JSON response here
-                        Log.e("ответ","все ок " + response.toString());
-                    }
-
+                }){
                     @Override
-                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                        Log.e("ответ", "не ок " + errorResponse.toString());
-                    }*/
-                });
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> map = new HashMap<>();
+                        map.put("file_upload", listImages.get(0));
+                        return map;
+                    }
+                };
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                queue.add(request);
             }
         });
 
@@ -125,32 +95,6 @@ public class PhotoSenderActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 photoEasy.startActivityForResult(PhotoSenderActivity.this);
-            }
-        });
-
-
-        String url = "https://172.16.0.227:600/api/upload_file";
-        AsyncHttpClient client = new AsyncHttpClient(true, 80, 443);
-        RequestParams params = new RequestParams();
-        params.put("q", "android");
-        params.put("rsz", "8");
-        client.get(url, params, new TextHttpResponseHandler() {
-            /*@Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                // Root JSON in response is an dictionary i.e { "data : [ ... ] }
-                // Handle resulting parsed JSON response here
-                Log.e("ответ","все ок");
-            }*/
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String res, Throwable t) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                Log.e("ответ","не ок");
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                Log.e("ответ","все ок");
             }
         });
 
@@ -173,15 +117,16 @@ public class PhotoSenderActivity extends AppCompatActivity {
                     ByteArrayOutputStream bos = new ByteArrayOutputStream();
                     bitmap.compress(Bitmap.CompressFormat.JPEG, 100 /*ignored for PNG*/, bos);
                     byte[] bitmapdata = bos.toByteArray();
+                    encodeImageString = android.util.Base64.encodeToString(bitmapdata, Base64.DEFAULT);
 
-                    FileOutputStream fos = new FileOutputStream(f);
+                    /*FileOutputStream fos = new FileOutputStream(f);
                     fos.write(bitmapdata);
                     fos.flush();
-                    fos.close();
+                    fos.close();*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                listImages.add(f);
+                listImages.add(encodeImageString);
                 //Log.e("проверка листа", listImages.size() + "" + f.toString());
                 sendPhoto.setVisibility(View.VISIBLE);
             }
